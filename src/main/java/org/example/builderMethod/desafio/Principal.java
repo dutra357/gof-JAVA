db6fc3b3bfaeaaa4e56578edf9897190df023074
David Alves Dutra <dadutra@hotmail.com>
2025-02-25 20:02:56 -0300

    BUILDER: Desafio. Calculo de imposto ok. Aguardando Factory para outros tipos de nota - PJ/PF.

--- src/main/java/org/example/builderMethod/desafio/Principal.java
+++ src/main/java/org/example/builderMethod/desafio/Principal.java
@@ -1,8 +1,18 @@
 package org.example.builderMethod.desafio;
 
+import org.example.builderMethod.desafio.builder.NotaFiscalBuilder;
+import org.example.builderMethod.desafio.client.NotaFiscal;
+
 public class Principal {
 
     public static void main(String[] args) {
 
+        NotaFiscal notaFiscal = new NotaFiscalBuilder()
+                .comNumero("123456789-0")
+                .comDataString("10/10/2024")
+                .comItemPedidoValorString("Televisão LCD", 1, "1000.00")
+                .builder();
+
+        System.out.println(notaFiscal.calcularImposto());
     }
 }
--- src/main/java/org/example/builderMethod/desafio/client/NotaFiscal.java
+++ src/main/java/org/example/builderMethod/desafio/client/NotaFiscal.java
@@ -1,5 +1,7 @@
 package org.example.builderMethod.desafio.client;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +16,16 @@ public class NotaFiscal {
     public NotaFiscal(String numero, LocalDate data) {
         this.numero = numero;
         this.data = data;
+    }
+
+    public BigDecimal calcularImposto() {
+        BigDecimal total = BigDecimal.ZERO;
+
+        for (ItemProduto item : this.itensProduto) {
+            total = total.add((item.getValor().multiply(new BigDecimal(item.getQuantidade()))));
+        }
+
+        return total.multiply(new BigDecimal("0.04"), new MathContext(2));
     }
 
     public String getNumero() {
