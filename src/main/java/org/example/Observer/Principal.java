22d28e606a91188a8150e5cb56e1cbf45384b38a
David Alves Dutra <dadutra@hotmail.com>
2025-03-13 17:49:43 -0300

    OBSERVER: Iniciando pattern Observer.

---
+++ src/main/java/org/example/Observer/Principal.java
@@ -0,0 +1,14 @@
+package org.example.Observer;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        /**
+         * Pattern Observer: Define uma dependência 'um para muitos'
+         * entre objetos, de modo que, quando um objeto muda de estado,
+         * todos os seus dependentes são automaticamente notificados e
+         * atualizados.
+         */
+    }
+}
